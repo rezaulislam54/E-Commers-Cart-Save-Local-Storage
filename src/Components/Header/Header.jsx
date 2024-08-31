0e6ddf9418a7a81a677df7b17431a008e08d7c1d
rezaulislam54 <rezaulislam5490@gmail.com>
2024-09-01 00:34:01 +0600

    some change

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,15 +9,15 @@ const Header = () => {
 
     return (
         <header>
-            <nav className="flex justify-between items-center h-16 px-4 shadow-lg">      
+            <nav className="flex justify-between items-center h-16 px-4 shadow-lg bg-[#008080] text-white">      
                 <div onClick={()=> setopen(!open)} className={`text-2xl  md:hidden ${open? 'true' : 'folse'}`}>
                     {
                         open===true? <RxCross2></RxCross2>  :<AiOutlineMenu></AiOutlineMenu>
                     }
                 </div>
-                <img className="h-10" src="https://res.cloudinary.com/dvp64j4a3/image/upload/c_crop,w_180,h_70/v1724998491/logo_vgddsp.jpg" alt="" />
+                <img className="h-10 hidden md:flex" src="https://res.cloudinary.com/dvp64j4a3/image/upload/c_crop,w_180,h_70/v1724998491/logo_vgddsp.jpg" alt="" />
 
-                <ul className={` absolute md:static md:flex list-none md:gap-x-4 bg-white p-5 duration-1000 lg:gap-x-7 font-bold text-xl 
+                <ul className={` absolute bg-[#008080] pl  md:static md:flex list-none md:gap-x-4 p-5 duration-1000 lg:gap-x-7 font-bold text-xl 
                      ${open?'top-16': '-top-52'}`}>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
@@ -26,15 +26,17 @@ const Header = () => {
                     <li><a href="#">Contact</a></li> 
                 </ul>
 
-                <div className="md:flex items-center gap-x-4 ">
-                    <div className="tooltip bg-gray-100 border border-black border-opacity-20 w-10 h-10 rounded-full p-3 flex  items-center justify-center" data-tip="User">
-                        <FaUserPlus></FaUserPlus>
-                    </div>
-                    <div className="tooltip hidden bg-gray-100 border border-black border-opacity-20 w-10 h-10 rounded-full p-3 md:flex  items-center justify-center" data-tip="User">
-                        <FaCartPlus></FaCartPlus>
+                <div className="flex items-center gap-x-4 text-black">
+
+                    <div className="tooltip text-2xl bg-gray-100 border border-black border-opacity-20 w-10 h-10 rounded-full  flex p-1  items-center justify-center" data-tip="User">
+                        <a href="#"><FaCartPlus></FaCartPlus></a>
                         </div>
-                    <div className="tooltip hidden bg-gray-100 border border-black border-opacity-20 w-10 h-10 rounded-full p-3 md:flex  items-center justify-center" data-tip="User">
-                        <FaHeart ></FaHeart>
+                    <div className="tooltip hidden text-2xl bg-gray-100 border border-black border-opacity-20 w-10 h-10 rounded-full p-1 md:flex  items-center justify-center" data-tip="User">
+                        <a href="#"><FaHeart ></FaHeart></a>
+                    </div>
+
+                    <div className="tooltip bg-gray-100 border text-2xl border-black border-opacity-20 w-10 h-10 rounded-full p-1 flex  items-center justify-center" data-tip="User">
+                        <a href="#"><FaUserPlus></FaUserPlus></a>
                     </div>
                     
                 </div>
